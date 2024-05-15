ce989c60c14a9ca6c7c24393a857236d55978efb
lainodHO <133787146+lainodHO@users.noreply.github.com>
2024-05-14 23:43:36 -0600

    Fronted correcciones

---
+++ public/mainClient.js
@@ -0,0 +1,37 @@
+// main.js (cliente)
+var socket = io.connect('http://localhost:3003/');
+
+  // Escuchar un evento del servidor
+  socket.on('respueta', function (mensaje) {
+    console.log('Respuesta del servidor:', data);
+  });
+
+// Manejar el evento 'respuesta' del servidor
+socket.on('respuesta', function(messages){
+  var messageList = document.getElementById('message-list');
+  messageList.innerHTML = ''; // Limpiar la lista de mensajes antes de actualizarla
+  
+  // Iterar sobre los mensajes y agregarlos al HTML
+  messages.forEach(function(message){
+      var html = `<div class="message">
+                      <strong>${message.autor}</strong>
+                      <em>${message.texto}</em>
+                  </div>`;
+      messageList.innerHTML += html;
+         // Envía los datos del mensaje como console.log
+    socket.emit('nuevoMensaje', { autor: message.autor, texto: message.texto });
+    console.log('Mensaje enviado al servidor:', { autor: message.autor, texto: message.texto });
+    
+  });
+});
+
+// Función para enviar un nuevo mensaje al servidor
+function addMessage(e){
+  var payload = {
+      autor: document.getElementById('username').value,
+      texto: document.getElementById('texto').value
+  };
+  socket.emit('new-message', payload);
+  document.getElementById('texto').value = ''; // Limpiar el campo de texto después de enviar el mensaje
+  return false; // Para evitar que el formulario se envíe
+}
--- server/app.js
+++ server/app.js
@@ -41,7 +41,7 @@ routeConfig(app);
 
 // Handling not found routes
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, '../public', '404.html'));
 });
 
 
@@ -82,7 +82,7 @@ io.on('connection', function (socket) {
 
 // Crear servidor HTTPS
  // Cambia 3003 por el puerto que quieras
- app.listen(PORT, () => {
+ server.listen(PORT, () => {
      console.log(`Server running at http://localhost:${PORT}/`);
  });
  
